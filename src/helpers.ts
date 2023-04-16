98dce54e68328dcd4b869da9aeca7fa9caaf8007
Cerallin <cerallin@cerallin.top>
2023-04-16 16:20:04 +0800

    chore: sort items
    Signed-off-by: Cerallin <cerallin@cerallin.top>

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,3 +1,5 @@
+import Hexo = require("hexo");
+
 class Options {
     category: "anime" | "book" | "comic" | "music";
     limit: number = 3;
@@ -23,5 +25,24 @@ hexo.on('ready', function () {
 
     hexo.extend.helper.register('lang_code', function (code) {
         return lang.of(code)
+    })
+});
+
+hexo.extend.helper.register('posts_sort', function (posts: Hexo.Locals.Post[]) {
+    return posts.sort(function (a, b) {
+        const post_meta = hexo.extend.helper.get('post_meta');
+        const moment = hexo.extend.helper.get('moment');
+
+        const moments = [
+            post_meta(a),
+            post_meta(b),
+        ].map(meta => moment(meta.date, 'YYYY-MM-DD'));
+
+        if (moments[0].isBefore(moments[1]))
+            return 1;
+        else if (moments[1].isBefore(moments[0]))
+            return -1;
+        else
+            return 0;
     })
 });
